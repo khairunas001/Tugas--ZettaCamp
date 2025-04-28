cf653f02029d826f15443c453408323480c65326
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-04-28 23:41:04 +0700

    Tugas-ZettaCamp

---
+++ san_easy_max-word-of-sentences.js
@@ -0,0 +1,27 @@
+/**
+ * Direction:
+ * Find maximum number of words from given sentences.
+ *
+ * Expected:
+ * 23
+ */
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+  'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+  'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+];
+
+function result(sentences) {
+  // your code here
+  for (const sentence of sentences)  {
+    let maxWord = 0;
+    const wordCount = sentence.split(' ').filter(word => word.trim() !== '').length;
+    if(wordCount > maxWord) {
+      maxWord = wordCount;
+    }
+    console.log(maxWord);
+    return maxWord;
+  }
+}
+
+console.log(result(sentences));
